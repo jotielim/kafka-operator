8de6ba7bb3cfcd391958a716c33beeee28e32fa3
Yonathan Lim <yonathan.lim@hitachivantara.com>
2020-02-26 17:34:46 -0800

    wip

--- pkg/resources/kafka/kafka.go
+++ pkg/resources/kafka/kafka.go
@@ -565,47 +565,49 @@ func (r *Reconciler) reconcileKafkaPod(log logr.Logger, desiredPod *corev1.Pod) 
 				log.Info(fmt.Sprintf("lbIPs: %v", lbIPs))
 				log.Info("**** end get node by name")
 
-				//log.Info("**** get configmap")
-				//configMap := &corev1.ConfigMap{}
-				//err = r.Client.Get(context.Background(), client.ObjectKey{
-				//	Namespace: r.KafkaCluster.Namespace,
-				//	Name:      fmt.Sprintf(brokerConfigTemplate+"-%s", r.KafkaCluster.Name, brokerId),
-				//}, configMap)
-				//if err != nil {
-				//	log.Error(err, "Unable to get configMap")
-				//}
-				//log.Info(fmt.Sprintf("broker-config: %s", configMap.Data["broker-config"]))
+				log.Info("**** get configmap")
+				configMap := &corev1.ConfigMap{}
+				err = r.Client.Get(context.Background(), client.ObjectKey{
+					Namespace: r.KafkaCluster.Namespace,
+					Name:      fmt.Sprintf(brokerConfigTemplate+"-%s", r.KafkaCluster.Name, brokerId),
+				}, configMap)
+				if err != nil {
+					log.Error(err, "Unable to get configMap")
+				}
+				log.Info(fmt.Sprintf("broker-config: %s", configMap.Data["broker-config"]))
 
-				// We need to grab names for servers and client in case user is enabling ACLs
-				// That way we can continue to manage topics and users
-				serverPass, clientPass, superUsers, err := r.getServerAndClientDetails()
-				if err != nil {
-					return err
-				}
-				var broker v1beta1.Broker
-				for _, b := range r.KafkaCluster.Spec.Brokers {
-					log.Info(fmt.Sprintf("strconv.Itoa(int(b.Id)): %s, b.Id: %s, brokerId: %s", strconv.Itoa(int(b.Id)), b.Id, brokerId))
-					if strconv.Itoa(int(b.Id)) == brokerId {
-						log.Info(fmt.Sprintf("b.Id: %s, brokerId: %s", b.Id, brokerId))
-						broker = b
-						break
+				if !strings.Contains(configMap.Data["broker-config"], lbIPs[0]) {
+					// We need to grab names for servers and client in case user is enabling ACLs
+					// That way we can continue to manage topics and users
+					serverPass, clientPass, superUsers, err := r.getServerAndClientDetails()
+					if err != nil {
+						return err
 					}
+					var broker v1beta1.Broker
+					for _, b := range r.KafkaCluster.Spec.Brokers {
+						log.Info(fmt.Sprintf("strconv.Itoa(int(b.Id)): %s, b.Id: %s, brokerId: %s", strconv.Itoa(int(b.Id)), b.Id, brokerId))
+						if strconv.Itoa(int(b.Id)) == brokerId {
+							log.Info(fmt.Sprintf("b.Id: %s, brokerId: %s", b.Id, brokerId))
+							broker = b
+							break
+						}
+					}
+					brokerConfig, err := util.GetBrokerConfig(broker, r.KafkaCluster.Spec)
+					if err != nil {
+						log.Error(err, "unable to get broker config")
+						return err
+					}
+					log.Info(fmt.Sprintf("broker: %v", broker))
+					log.Info(fmt.Sprintf("brokerConfig: %v", brokerConfig))
+					typeMeta := r.KafkaCluster.TypeMeta
+					o := r.configMap(broker.Id, brokerConfig, lbIPs, serverPass, clientPass, superUsers, log)
+					err = r.Client.Update(context.TODO(), o)
+					//err = k8sutil.Reconcile(log, r.Client, o, r.KafkaCluster)
+					if err != nil {
+						return errors.WrapIfWithDetails(err, "failed to reconcile resource", "resource", o.GetObjectKind().GroupVersionKind())
+					}
+					r.KafkaCluster.TypeMeta = typeMeta
 				}
-				brokerConfig, err := util.GetBrokerConfig(broker, r.KafkaCluster.Spec)
-				if err != nil {
-					log.Error(err, "unable to get broker config")
-					return err
-				}
-				log.Info(fmt.Sprintf("broker: %v", broker))
-				log.Info(fmt.Sprintf("brokerConfig: %v", brokerConfig))
-				typeMeta := r.KafkaCluster.TypeMeta
-				o := r.configMap(broker.Id, brokerConfig, lbIPs, serverPass, clientPass, superUsers, log)
-				err = r.Client.Update(context.TODO(), o)
-				//err = k8sutil.Reconcile(log, r.Client, o, r.KafkaCluster)
-				if err != nil {
-					return errors.WrapIfWithDetails(err, "failed to reconcile resource", "resource", o.GetObjectKind().GroupVersionKind())
-				}
-				r.KafkaCluster.TypeMeta = typeMeta
 			}
 
 			if r.KafkaCluster.Spec.RackAwareness != nil {
